9d41eb18140385486ddca8f728b7471aff395235
dff9th <dff9th@gmail.com>
2022-01-05 01:48:50 +0900

    Update sample dependencies

--- sample/src/HoverableLabel.ts
+++ sample/src/HoverableLabel.ts
@@ -1,28 +1,27 @@
 import * as hover from "@akashic-extension/akashic-hover-plugin";
 
 export class HoverableLabel extends g.Label implements hover.HoverableE {
-	hoverable: boolean = true;
-	touchable: boolean = true;
-	hovered: g.Trigger<void> = new g.Trigger<void>();
-	unhovered: g.Trigger<void> = new g.Trigger<void>();
-	_text: string;
+  hoverable: boolean = true;
+  hovered: g.Trigger<void> = new g.Trigger<void>();
+  unhovered: g.Trigger<void> = new g.Trigger<void>();
+  _text: string;
 
-	constructor(param: g.LabelParameterObject) {
-		super(param);
-		this._text = this.text;
-		this.hovered.add(this.onHovered, this);
-		this.unhovered.add(this.onUnhovered, this);
-	}
+  constructor(param: g.LabelParameterObject) {
+    super(param);
+    this._text = this.text;
+    this.hovered.add(this.onHovered, this);
+    this.unhovered.add(this.onUnhovered, this);
+  }
 
-	onHovered(): void {
-		this.text = "hover!";
-		this.textColor = "#f00";
-		this.invalidate();
-	}
+  onHovered(): void {
+    this.text = "hover!";
+    this.textColor = "#f00";
+    this.invalidate();
+  }
 
-	onUnhovered(): void {
-		this.text = this._text;
-		this.textColor = "#000";
-		this.invalidate();
-	}
+  onUnhovered(): void {
+    this.text = this._text;
+    this.textColor = "#000";
+    this.invalidate();
+  }
 }
--- sample/src/main.ts
+++ sample/src/main.ts
@@ -3,59 +3,61 @@ import { HoverableLabel } from "./HoverableLabel";
 import { HoverableRect } from "./HoverableRect";
 
 export function main(param: g.GameMainParameterObject): void {
-	const scene = new g.Scene({game: g.game, assetIds: ["aco"]});
-	scene.onLoad.add(() => {
-		const font = new g.DynamicFont({
-			game: g.game,
-			size: 40,
-			fontFamily: "sans-serif"
-		});
+  const scene = new g.Scene({ game: g.game, assetIds: ["aco"] });
+  scene.onLoad.add(() => {
+    const font = new g.DynamicFont({
+      game: g.game,
+      size: 40,
+      fontFamily: "sans-serif",
+    });
 
-		for (let i = 0; i < 5; i++) {
-			(() => {
-				const aco = new g.FrameSprite({
-					scene: scene,
-					src: scene.asset.getImageById("aco"),
-					x: 5,
-					y: i * 50,
-					width: 32,
-					height: 48,
-					frames: [5, 6, 7, 6]
-				});
-				const hoverableAco = hover.Converter.asHoverable(aco);
-				hoverableAco.hovered.add(() => {
-					aco.start();
-				});
-				hoverableAco.unhovered.add(() => {
-					aco.stop();
-				});
-				hoverableAco.title = `aco${i + 1}: こんにちわ！`;
+    for (let i = 0; i < 5; i++) {
+      (() => {
+        const aco = new g.FrameSprite({
+          scene: scene,
+          src: scene.asset.getImageById("aco"),
+          x: 5,
+          y: i * 50,
+          width: 32,
+          height: 48,
+          frames: [5, 6, 7, 6],
+        });
+        const hoverableAco = hover.Converter.asHoverable(aco);
+        hoverableAco.hovered.add(() => {
+          aco.start();
+        });
+        hoverableAco.unhovered.add(() => {
+          aco.stop();
+        });
+        hoverableAco.title = `aco${i + 1}: こんにちわ！`;
 
-				const rect = new HoverableRect({
-					scene: scene,
-					x: 40,
-					y: i * 50 + 20,
-					width: 20,
-					height: 20,
-					cssColor: "#000"
-				});
+        const rect = new HoverableRect({
+          scene: scene,
+          x: 40,
+          y: i * 50 + 20,
+          width: 20,
+          height: 20,
+          cssColor: "#000",
+          touchable: true,
+        });
 
-				const label = new HoverableLabel({
-					scene: scene,
-					x: 65,
-					y: i * 50 + 18,
-					text: `test${i + 1}`,
-					fontSize: 20,
-					font: font
-				});
+        const label = new HoverableLabel({
+          scene: scene,
+          x: 65,
+          y: i * 50 + 18,
+          text: `test${i + 1}`,
+          fontSize: 20,
+          font: font,
+          touchable: true,
+        });
 
-				scene.append(aco);
-				scene.append(rect);
-				scene.append(label);
-			})();
-		}
-	});
-	g.game.pushScene(scene);
+        scene.append(aco);
+        scene.append(rect);
+        scene.append(label);
+      })();
+    }
+  });
+  g.game.pushScene(scene);
 }
 
 module.exports = main;
